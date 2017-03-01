b509c29725784d45c964ba70330fd447c65ba3b9
KalleV <kvirtaneva@gmail.com>
2017-03-01 16:11:45 -0700

    Update UI template for CJS format (Webpack) instead of RequireJS
    SHELL-793 #resolves

--- templates/ui-package/ui/home/index.js
+++ templates/ui-package/ui/home/index.js
@@ -1,6 +1,3 @@
 // File that brings module files together and is referenced in app.js
-define([
-    './module',
-    './controller'
-], function () {
-});
+require('./module');
+require('./controller');
