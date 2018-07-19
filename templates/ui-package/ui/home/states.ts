6c901bfd1e847465be7e9e210b674e070d466976
rafaelcalpena <rafael.calpena@gmail.com>
2018-07-19 11:28:46 -0300

    chore: remove rolesRequired for home route

--- templates/ui-package/ui/home/states.ts
+++ templates/ui-package/ui/home/states.ts
@@ -3,9 +3,6 @@ import {HomeComponent} from './home.component';
 export const states = [
     {
         name: 'home',
-        data: {
-            rolesRequired: ['user', 'staff', 'admin']
-        },
         url: '/<%= appNameSlug %>/home',
         component: HomeComponent
     }
