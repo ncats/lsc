7994a35f183324e6be326f55324b4949da5530c0
Kalle Virtaneva <kvirtaneva@wayfair.com>
2019-08-15 12:00:54 -0400

    feat(logging): add timestamp to console logs

--- lib/log/logger.ts
+++ lib/log/logger.ts
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const { createLogger, format, transports } = require('winston');
+import { createLogger, format, transports } from "winston";
 import { getPackageManifest, getPackageName } from "../cli";
-import path = require('path');
-import fs = require('fs');
-import _ = require('lodash');
+import path = require("path");
+import fs = require("fs");
+import _ = require("lodash");
 
 /*
  * LSC's logging configuration module.
@@ -15,107 +15,109 @@ import _ = require('lodash');
  * logger.error('an error');
  * logger.warn('a warning');
  */
-const FluentTransport = require('fluent-logger').support.winstonTransport();
+const FluentTransport = require("fluent-logger").support.winstonTransport();
 
 function mkdirSync(path: string): void {
-    try {
-        fs.mkdirSync(path);
-    } catch (error) {
-        if (error.code !== 'EEXIST') {
-            throw error;
-        }
+  try {
+    fs.mkdirSync(path);
+  } catch (error) {
+    if (error.code !== "EEXIST") {
+      throw error;
     }
+  }
 }
 interface LoggerOptions {
-    fluentD?: {
-        host?: string
-        port?: number
-        timeout?: number
-        tag?: string
-    },
-    format?: {
-        timestamp?: boolean,
-        json?: boolean,
-        colorize?: boolean
-    },
-    cwd?: string
-    logDirectory?: string
+  fluentD?: {
+    host?: string;
+    port?: number;
+    timeout?: number;
+    tag?: string;
+  };
+  format?: {
+    json?: boolean;
+    colorize?: boolean;
+  };
+  cwd?: string;
+  logDirectory?: string;
 }
 // default format for Console, error at winston: if not json is specified is not logging in console
-const loggerFormat = format.printf(({level, message }) => {
-    return `${level}: ${message}`;
+const loggerFormat = format.printf(({ timestamp, level, message }) => {
+  return `${timestamp} ${level}: ${message}`;
 });
 export const Logger = (options: LoggerOptions) => {
+  options = _.defaultsDeep(options || {}, {
+    fluentD: {},
+    logDirectory: null,
+    format: {
+      json: false,
+      colorize: false
+    },
+    cwd: process.cwd()
+  });
 
-    options = _.defaultsDeep(options || {}, {
-        fluentD: {},
-        logDirectory: null,
-        format: {
-            timestamp: true,
-            json: false,
-            colorize: false
-        },
-        cwd: process.cwd()
+  // Defining custom formats
+  const styleFormats = [
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    })
+  ];
+
+  // special case: if json is not defined , use by default the custom format
+  // Example: https://github.com/winstonjs/winston/blob/master/examples/custom-timestamp.js
+  if (!options.format.json) {
+    styleFormats.push(loggerFormat);
+  } else {
+    styleFormats.push(format.json());
+  }
+  // TODO: enable dynamic formats and parameters , for now it is only using basic types with no parameters
+  for (const key of Object.keys(options.format)) {
+    if (options.format[key]) {
+      styleFormats.push(format[key]());
+    }
+  }
+  const logger = createLogger({
+    format: format.combine(...styleFormats)
+  });
+
+  // Adding a new Console transport
+  logger.add(new transports.Console());
+  if (options.logDirectory) {
+    // Create the log directory if it doesn't exist
+    mkdirSync(options.logDirectory);
+
+    logger.add(
+      new transports.File({
+        filename: path.resolve(options.logDirectory, "app.log"),
+        maxFiles: 5,
+        maxsize: 10485760,
+        level: "info"
+      })
+    );
+  }
+  // Adding fluentD transport
+  if (!_.isEmpty(options.fluentD)) {
+    let name: string = getPackageName(getPackageManifest(options.cwd));
+
+    options = _.defaultsDeep(options, {
+      fluentD: {
+        host: "localhost",
+        port: 24224,
+        timeout: 3.0,
+        tag: name || "labshare"
+      }
     });
 
-    // Defining custom formats 
-    const styleFormats = [];
-
-    // special case: if json is not defined , use by default the custom format 
-    if (options.format.json === false) {
-        // going to global
-        styleFormats.push( loggerFormat);
-    }
-    else{
-        styleFormats.push(format.json());
-    }
-    // TODO: enable dynamic formats and parameters , for  now it is only using basic types with no parameters    
-    _.forEach(['colorize', 'timestamp'], (value) => {
-        if (_.get(options.format, `${value}`) === true) {
-            styleFormats.push(format[`${value}`]());
+    logger.add(new FluentTransport(options.fluentD.tag, options.fluentD));
+  }
+  // Workaround to support the Morgan request logging middleware
+  return _.assign(logger, {
+    stream: () => {
+      let self: any = this;
+      return {
+        write(options?: any): void {
+          self.info(options);
         }
-    });
-    const _logger = createLogger({
-        format: format.combine(...styleFormats)
-    });
-    // Adding a new Console transport
-    _logger.add(new transports.Console());
-    if (options.logDirectory) {
-        // Create the log directory if it doesn't exist
-        mkdirSync(options.logDirectory);
-
-        _logger.add(new transports.File({
-            filename: path.resolve(options.logDirectory, 'app.log'),
-            maxFiles: 5,
-            maxsize: 10485760,
-            level: 'info'
-        }));
+      };
     }
-    // Adding fluentD transport
-    if (!_.isEmpty(options.fluentD)) {
-        let name: string = getPackageName(getPackageManifest(options.cwd));
-
-        options = _.defaultsDeep(options, {
-            fluentD: {
-                host: 'localhost',
-                port: 24224,
-                timeout: 3.0,
-                tag: name || 'labshare'
-            }
-        });
-
-        _logger.add(new FluentTransport(options.fluentD.tag, options.fluentD));
-    }
-    // Workaround to support the Morgan request logging middleware
-    return _.assign(_logger, {
-        stream: () => {
-            let self: any = this;
-            return {
-                write(options?: any): void {
-                    self.info(options);
-                }
-            }
-        }
-    });;
-
-}
+  });
+};
--- test/lib/unit/cli/start_spec.ts
+++ test/lib/unit/cli/start_spec.ts
@@ -1,26 +1,22 @@
-import {checkVersion} from "../../../../lib/check-self-update";
-import {start} from "../../../../lib/cli";
+import { checkVersion } from "../../../../lib/check-self-update";
+import { start } from "../../../../lib/cli";
 
-describe('.start()', () => {
+describe(".start()", () => {
+  afterEach(() => {
+    delete global.LabShare;
+  });
 
-    afterEach(() => {
-        delete global.LabShare;
+  it("bootstraps the CLI", () => {
+    expect(global.LabShare).toBeUndefined();
+
+    start({});
+
+    expect(global.LabShare).toBeDefined();
+  });
+
+  describe(".checkVersion()", () => {
+    it('checks if the installed version of "package-name" needs to be updated', async () => {
+      await checkVersion({ name: "lsc", logger: console.log });
     });
-
-    it('bootstraps the CLI', () => {
-        expect(global.LabShare).toBeUndefined();
-
-        start({});
-
-        expect(global.LabShare).toBeDefined();
-    });
-
-    describe('.checkVersion()', () => {
-
-        it('checks if the installed version of "package-name" needs to be updated', async () => {
-            await checkVersion({name: 'lsc', logger: console.log});
-        });
-
-    });
-
+  });
 });
