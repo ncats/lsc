63d4790d9cdc26298b58612e95a73da2da48338b
Jamil Omar <jfalconi63@hotmail.com>
2019-11-26 11:26:52 -0500

    fix: json true

--- lib/check-self-update/fetch-module-registry-info.ts
+++ lib/check-self-update/fetch-module-registry-info.ts
@@ -9,15 +9,12 @@ export function fetchModuleRegistryInfo(name: string) {
     const url = [REGISTRY_URL, name, 'latest'].join('/');
 
     return new Promise((resolve, reject) => {
-        request({ url, method: 'GET'}, (error, response, body) => {
+        request({ url, method: 'GET' , json:true}, (error, response, body) => {
             if (error) {
                 reject(error);
                 return;
             }
-            if (body)
-                resolve(JSON.parse(body));
-            else
-                resolve();
+            resolve(body);
         });
     });
 }
